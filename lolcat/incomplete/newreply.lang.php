9d2e6bb98931dd9437390d12eb8694558be9f38e
kswis <simpsky97@comcast.net>
2013-02-08 19:47:02 -0500

    Adds various new phrases.

---
+++ lolcat/incomplete/newreply.lang.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * MyBB 1.6 English Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ * 
+ * $Id$
+ */
+
+$l['nav_newreply'] = "NEW POST";
+
+$l['post_reply_to'] = "NEW POST IN {1}";
+$l['post_new_reply'] = "MAKE NEW POST";
+$l['reply_to'] = "POST IN {1}";
+$l['post_subject'] = "Post Subject:";
+$l['your_message'] = "UR POST:";
+$l['post_options'] = "Post Options:";
+$l['options_sig'] = "<strong>SIGNACHUR:</strong> INCLUDE SIGNACHUR. (ONLY 4 REGISTURED MEMBURS)";
+$l['options_emailnotify'] = "<strong>EMAIL:</strong> RECEIV AN EMAIL WHENEVR THAR IZ NEW REPLY. (REGISTERD USERS ONLY)";
+$l['options_disablesmilies'] = "<strong>DISABEL SMILIEZ:</strong> DISABLE SMILIEZ FRUM SHOWIN IN DIS POST.";
+$l['post_reply'] = "POST!!";
+$l['preview_post'] = "PREVIEW POST";
+$l['mod_options'] ="MODERATER OPSHUNZ:";
+$l['close_thread'] = "<strong>KILL THRED</strong>: U CAN'T POST IN IT.";
+$l['stick_thread'] = "<strong>Stick Thread:</strong> stick this thread to the top of the forum.";
+$l['forum_rules'] = "{1} - Rules";
+$l['thread_review'] = "Thread Review (Newest First)";
+$l['thread_review_more'] = "This thread has more than {1} replies. <a href=\"{2}\">Read the whole thread.</a>";
+$l['posted_by'] = "Posted by";
+$l['draft_saved'] = "The new post has successfully been saved as a draft.<br />You will now be taken to your draft listing.";
+$l['image_verification'] = "Image Verification";
+$l['verification_note'] = "U NEED 2 WRITE THE TEXT IN THE BOX. ITS FUR UR OWN GUD.";
+$l['verification_subnote'] = "(case insensitive)";
+$l['invalid_captcha'] = "U ROTE THE RONG CODE. WRITE IT AS U SEE IT, PLZ.";
+$l['error_post_already_submitted'] = "U'VE POSTED THIS ALREDY. GO TO THE THRED 2 SEE IT, PLZ.";
+$l['multiquote_external_one'] = "You have selected one post outside of this thread.";
+$l['multiquote_external'] = "You have selected {1} posts outside of this thread.";
+$l['multiquote_external_one_deselect'] = "deselect this post";
+$l['multiquote_external_deselect'] = "deselect these posts";
+$l['multiquote_external_one_quote'] = "Quote this post too";
+$l['multiquote_external_quote'] = "Quote these posts too";
+
+$l['redirect_newreply'] = "Thank you, your reply has been posted.";
+$l['redirect_newreply_moderation'] = "The administrator has specified that all new posts require moderation. You will now be returned to the thread.";
+$l['redirect_newreply_post'] = "<br />You will now be taken to your post.";
+$l['redirect_newreplyerror'] = "Sorry, but your reply has been rejected for lack of content. <br />You will now be returned to the thread.";
+$l['redirect_threadclosed'] = "You cannot post replies in this thread because it has been closed by a moderator.";
+$l['error_post_noperms'] = "You don't have permission to edit this draft.";
+?>
---
+++ lolcat/showteam.lang.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * MyBB 1.6 English Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ * 
+ * $Id$
+ */
+
+$l['nav_showteam'] = "TEH TEAM";
+$l['forum_team'] = "TEH TEAM";
+$l['moderators'] = "MODS";
+$l['mod_username'] = "USRNAME";
+$l['mod_forums'] = "FORUM(Z)";
+$l['mod_email'] = "EMAILZ";
+$l['mod_pm'] = "MAILZ";
+$l['uname'] = "USRNAME";
+$l['email'] = "EMAILZ";
+$l['pm'] = "MAILZ";
+
+$l['group_leaders'] = "LEADR(Z)";
+$l['group_members'] = "PIPLZ";
+
+$l['no_members'] = "NO ONE IN GROUP";
+
+$l['error_noteamstoshow'] = "THERE R NO ONE ON TEH TEAM.";
+?>
